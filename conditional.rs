b5feeb2c3317cecb1fb30c4aeff51c77f78085b0
Martin Mato <martin@zbytes.tech>
2025-08-04 16:53:47 +0100

    Learned conditionals

---
+++ conditional.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    let number = 15;
+    if number < 10 {
+        println!("The number is less than 10");
+    } 
+    else {
+        println!("The number is 10 or greater");
+    }
+}
