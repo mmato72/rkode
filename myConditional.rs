07099e1b0576ad36dd36370d5625a66549dcd36b
Martin Mato <martin@zbytes.tech>
2025-08-08 16:12:38 +0100

    True confession

---
+++ myConditional.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let number = 100000;
+
+    if number % 5 == 0 && number % 2  == 0 {
+        println!("God is in charge");
+    }
+    else if number % 5 == 0 {
+        println!("God is still in charge");
+    }
+    else {
+        println!("The sovereign Lord remains in charge");
+    }
+}
