fcc2871d75aa3ed985d107707b5c4bb9216e3a17
Martin Mato <martin@zbytes.tech>
2025-08-11 16:49:25 +0100

    Nested Loops learned

---
+++ nestedLoop.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut outer_count = 0;
+
+    'outer: loop {
+        println!("Outer count: {}", outer_count);
+
+        let mut inner_count = 0;
+        loop {
+            println!("Inner count: {}", inner_count);
+            inner_count += 1;
+
+            if inner_count == 2 {
+                break; // This breaks the inner loop
+            }
+
+            if outer_count == 3 {
+                break 'outer; // This breaks the outer loop
+            }
+        }
+        println!("------------------");
+        outer_count += 1;
+    }
+
+    println!("Outer loop ended at count: {}", outer_count);
+}
